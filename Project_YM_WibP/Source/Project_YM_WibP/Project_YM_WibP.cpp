37c5aa1c890f2923166e479219f342cdf481e85e
yumouWYS <1004246132@qq.com>
2026-03-16 22:42:26 +0800

    提交第一个项目

---
+++ Project_YM_WibP/Source/Project_YM_WibP/Project_YM_WibP.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Project_YM_WibP.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Project_YM_WibP, "Project_YM_WibP" );
